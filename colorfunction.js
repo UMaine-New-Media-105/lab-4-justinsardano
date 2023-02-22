9f2dc72431f4e5f678fa3664949019ba182fd5f7
justinsardano <124173790+justinsardano@users.noreply.github.com>
2023-02-22 10:35:22 -0500

    shapes with colors created from a function
    https://editor.p5js.org/justin.sardano/sketches/n-J8msgTW

---
+++ colorfunction.js
@@ -0,0 +1,27 @@
+function setup() {
+function setup(hsla) {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+
+  myTriangle(50,50,50,50,50,50,"red")
+  myCircle(0,0,0,"blue")
+ myArc(0,0,0,0,0,0,"purple")
+  myTriangle(50,50,50,50,50,50,('hsla(180, 100%, 50%, 0.5)'))
+  myCircle(0,0,0,('hsla(50,100%,50%,0.5)'))
+ myArc(0,0,0,0,0,0,('hsla(270,200%,40%,0.5)'))
+}
+function myTriangle(x1,y1,x2,y2,x3,y3,myColor){
+fill(myColor)
+  triangle (20,40,60,4,50,100)
+}
+function myCircle(x,y,diameter,myColor){
+  fill(myColor)
+  ellipse(100,100,100)
+}
+function myArc(x,y,x2,y2,x3,y3,myColor){
+ fill(myColor)
+  arc(300,300,100,100,100,10)
+}
